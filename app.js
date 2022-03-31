3975157762d5552d4c424935bd5c8740ef1c52d6
Abhishek Kumar Kamti <abhishekkamti723@gmail.com>
2022-03-31 08:23:42 +0530

    adding password in hashing form

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -21,7 +22,7 @@ const userDocSchema = new mongoose.Schema({
 });
 
 // let secret = process.env.SOME_LONG_UNGUESSABLE_STRING;
-userDocSchema.plugin(encrypt, {secret : process.env.SECRET, encryptedFields:['password']});
+// userDocSchema.plugin(encrypt, {secret : process.env.SECRET, encryptedFields:['password']});
 
 const User = new mongoose.model("User", userDocSchema);
 
@@ -42,7 +43,7 @@ app.post("/register", (req, res)=>{
 
     const newUser = new User({
         email: req.body.username, 
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save((err)=>{
@@ -58,7 +59,7 @@ app.post("/register", (req, res)=>{
 app.post("/login", (req, res)=>{
 
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email : username}, (err, foundUser)=>{
 
