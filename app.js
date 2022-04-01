2229aa55e91954c9ac7d996757f3f813b00c5569
Abhishek Kumar Kamti <abhishekkamti723@gmail.com>
2022-04-01 07:27:18 +0530

    adding registration feature

--- app.js
+++ app.js
@@ -6,15 +6,24 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 const md5 = require("md5");
-const bcrypt = require("bcrypt");
-
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+    secret : "Our little secret",
+    resave : false,
+    saveUninitialized: false 
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 mongoose.connect("mongodb://localhost:27017/userDB");
@@ -24,10 +33,18 @@ const userDocSchema = new mongoose.Schema({
     password : String
 });
 
+userDocSchema.plugin(passportLocalMongoose);
+ 
 // let secret = process.env.SOME_LONG_UNGUESSABLE_STRING;
 // userDocSchema.plugin(encrypt, {secret : process.env.SECRET, encryptedFields:['password']});
 
 const User = new mongoose.model("User", userDocSchema);
+// use static authenticate method of model in LocalStrategy
+passport.use(User.createStrategy());
+ 
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.get("/", (req, res)=>{
@@ -42,62 +59,39 @@ app.get("/register", (req, res)=>{
     res.render("register");
 });
 
+app.get("/secrets", (req, res)=>{
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }
+    else{
+        res.redirect("/login");
+    }
+});
+
 app.post("/register", (req, res)=>{
     
-    bcrypt.hash(req.body.password, saltRounds, (err, hash)=>{
-        
-        const newUser = new User({
-            email: req.body.username, 
-            password: hash  // md5(req.body.password)
-        });
-    
-        newUser.save((err)=>{
-            if(err){
+    User.register({username: req.body.username, active: false}, req.body.password, function(err, user) {
+        if (err) {  
+            console.log(err);
+        }
+        const authenticate = User.authenticate();
+        authenticate(user.username, req.body.password, function(err, result) {
+            if (err) {
                 console.log(err);
-            }
+            }   
             else{
                 res.render("secrets");
             }
-        });
-    });
-
-
-    
+            // Value 'result' is set to false. The user could not be authenticated since the user is not active
+          });
+      });
+       
 });
 
-app.post("/login", (req, res)=>{
+app.post("/login", passport.authenticate("local", { failureRedirect: "/login" }), (req, res)=>{
 
-    const username = req.body.username;
-    const password = req.body.password;
+    res.redirect('/secrets');
 
-    
-
-    User.findOne({email : username}, (err, foundUser)=>{
-
-        if(!err){
-            if(foundUser){
-                bcrypt.compare(password, foundUser.password, (err, result)=>{
-                    if(!err){
-                        if(result){
-                            res.render("secrets");
-                        }
-                        else{
-                            console.log("Password does not match !!!");
-                        }
-                    }
-                    else{
-                        console.log(err);
-                    }
-                });
-            }
-        }
-        else{
-            console.log(err);
-        }
-       
-
-       
-    });
 });
 
 
